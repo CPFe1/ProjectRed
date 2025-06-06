99c2e77097a7f0c15be9d54f62843c07150954d8
CPFe1 <214245144+CPFe1@users.noreply.github.com>
2025-06-06 23:18:15 +0200

    Restructuring of the fabrication module

---
+++ src/main/scala/mrtjp/projectred/fabrication/circuitparts/ArrayGateICPart.scala
@@ -0,0 +1,17 @@
+package mrtjp.projectred.fabrication.circuitparts
+
+
+class ArrayGateICPart
+  extends RedstoneGateICPart
+    with TComplexGateICPart
+    with TArrayGateICPart {
+  private var logic: ArrayGateICLogic = null
+
+  override def getLogic[T] = logic.asInstanceOf[T]
+
+  override def assertLogic() {
+    if (logic == null) logic = ArrayGateICLogic.create(this, subID)
+  }
+
+  override def getPartType = CircuitPartDefs.ArrayGate
+}
